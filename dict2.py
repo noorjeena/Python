f4576f72dfb3366bc017ea8ad3ee5fed81f07cb4
noorjeena <noorjeenashereef123@gmail.com>
2025-10-08 02:21:05 -0700

    Add files via upload

---
+++ dict2.py
@@ -0,0 +1,8 @@
+contact={"Anu":"9878985463","Teena":"8847856676"}
+contact1={"john":"89897667782","shreya":"9090876549"}
+print("contact : ",contact)
+print("contact : ",contact1)
+merged_contact=contact.copy()
+merged_contact.update(contact1)
+print("merged contacts : ")
+print(merged_contact)
